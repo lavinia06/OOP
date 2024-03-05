56279e72e287132b915dda3669dcfe716879775c
914_Ionel_Lavinia <67369509+lavinia06@users.noreply.github.com>
2024-03-05 12:15:48 +0200

    Create Repository.h

---
+++ Repository/Repository.h
@@ -0,0 +1,22 @@
+//
+// Created by Alexandra on 5/24/2021.
+//
+
+#pragma once
+#include "weather.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+class Repository {
+
+private:
+    vector<weather> elements;
+    void LoadData();
+
+public:
+    Repository();
+    ~Repository();
+    vector<weather> getAll();
+
+};
